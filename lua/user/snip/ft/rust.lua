c0f6bc03d9512f8b25e199b40f13976bb68dd271
PG12138 <tmd@tencent>
2022-07-15 15:04:19 +0800

    fix

--- lua/user/snip/ft/rust.lua
+++ lua/user/snip/ft/rust.lua
@@ -61,7 +61,7 @@ return {
         {}
     }}{}
     ]],
-    { i(1, "name"), i(2), i(3), ,i(4, "unimplemented!()"), i(0) }
+    { i(1, "name"), i(2), i(3), i(4, "unimplemented!()"), i(0) }
     )),
 
     s("struct", fmt(
