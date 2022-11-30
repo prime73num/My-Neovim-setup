ab19baaf9f6b8a07d9af2bbe8f28508f36f807c6
PG12138 <prime73num@163.com>
2022-11-30 13:18:50 +0800

    fix neotree

--- lua/user/neotree.lua
+++ lua/user/neotree.lua
@@ -288,6 +288,7 @@ local config = {
     follow_current_file = true, -- This will find and focus the file in the active buffer every
     -- time the current file is changed while the tree is open.
     group_empty_dirs = false, -- when true, empty folders will be grouped together
+    show_unloaded = false,
     commands = {
       floaterm = function(state)
         local node = state.tree:get_node()
